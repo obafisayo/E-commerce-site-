78bcc242e00222d009b32dd5de14596e840624d2
Okunola Olubanjo <okunolaolubanjo@gmail.com>
2024-07-07 01:23:17 +0100

    Added the arrow up icon

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { FaBeer } from 'react-icons/fa';
 import { FaMusic  } from 'react-icons/fa';
 import { FaCheckSquare } from 'react-icons/fa';
+import { FaArrowUp } from 'react-icons/fa';
 
 const MyFooter = () => {
   return (
-    <div className="flex flex-row gap-x-28 justify-center mt-10">
-      <div className='flex flex-col gap-1 items-center'>
+    <div className='flex flex-col gap-4 mt-16 mb-16'>
+      <div className="flex flex-row gap-x-28 justify-center mt-10">
+        <div className='flex flex-col gap-1 items-center'>
         <div className='w-20 h-20 border-8 rounded-full flex items-center justify-center'> <FaBeer className='text-2xl bg-black text-white' /></div>
         <h2 className="font-semibold">FREE AND FAST DELIVERY</h2>
         <p>Free delivery for all orders over $140</p>
@@ -26,6 +28,10 @@ const MyFooter = () => {
       <div className='w-20 h-20 border-8 rounded-full flex items-center justify-center'>< FaCheckSquare className='text-2xl'/></div>
         <h2  className="font-semibold">MONEY BACK GUARANTEE</h2>
         <p>We return money within 30days</p>
+      </div>
+      </div>
+      <div className='bg-[#F5F5F5] w-6 h-6 rounded-full  flex items-center justify-center  ml-auto mr-10'>
+        <FaArrowUp />
       </div>
     </div>
   )
