ca958cb28a1fe4710225022593f540c659f8cb27
obafisayo <sekaniisjoy@gmail.com>
2024-07-07 21:59:39 +0100

    removed the fuschia colored div

--- src/pages/LandingPage/home/Home.jsx
+++ src/pages/LandingPage/home/Home.jsx
@@ -6,10 +6,6 @@ const Home = () => {
     <main>
       <div className="container">
         <Herosection />
-        <div className="bg-fuchsia-400 h-[400px] flex items-center
-          text-center text-white font-semibold">
-          <h1 className="text-xl">This is the Home page</h1>
-        </div>
       </div>
     </main>
     )
